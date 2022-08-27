e84c612300e9e56fc0bd4bd097ed8b87d046c43b
adarsha14 <111411927+adarsha14@users.noreply.github.com>
2022-08-27 23:18:05 +0530

    Update ProductLoader.java

--- src/main/java/guru/springframework/bootstrap/ProductLoader.java
+++ src/main/java/guru/springframework/bootstrap/ProductLoader.java
@@ -28,18 +28,18 @@ public class ProductLoader implements ApplicationListener<ContextRefreshedEvent>
     public void onApplicationEvent(ContextRefreshedEvent event) {
 
         Product shirt = new Product();
-        shirt.setDescription("Spring Framework Guru Shirt");
+        shirt.setDescription("T-Shirt Red");
         shirt.setPrice(new BigDecimal("18.95"));
-        shirt.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_shirt-rf412049699c14ba5b68bb1c09182bfa2_8nax2_512.jpg");
-        shirt.setProductId("235268845711068308");
+        shirt.setImageUrl("https://cdn.shopify.com/s/files/1/0028/6559/4412/products/Angry-Birds-T-shirt-Red-The-Banyan-Tee-1654592163-_1.jpg?v=1654893464");
+        shirt.setProductId("2352");
         productRepository.save(shirt);
 
         log.info("Saved Shirt - id: " + shirt.getId());
 
         Product mug = new Product();
-        mug.setDescription("Spring Framework Guru Mug");
-        mug.setImageUrl("https://springframework.guru/wp-content/uploads/2015/04/spring_framework_guru_coffee_mug-r11e7694903c348e1a667dfd2f1474d95_x7j54_8byvr_512.jpg");
-        mug.setProductId("168639393495335947");
+        mug.setDescription("Mug");
+        mug.setImageUrl("https://i.etsystatic.com/23564917/r/il/60629b/2743767292/il_340x270.2743767292_nly8.jpg");
+        mug.setProductId("1689");
         mug.setPrice(new BigDecimal("11.95"));
         productRepository.save(mug);
 
